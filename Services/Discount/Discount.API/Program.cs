deb7660060325e919ee61335266b86c330a3c458
useraman123 <amank100003@gmail.com>
2025-05-04 17:26:21 +0530

    program.cs changes

--- Services/Discount/Discount.API/Program.cs
+++ Services/Discount/Discount.API/Program.cs
@@ -1,23 +1,54 @@
+using Discount.API.Services;
+using Discount.Application.Handler;
+using Discount.Core.Repositories;
+using Discount.Infrastructure.Extension;
+using Discount.Infrastructure.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//Register AutoMapper
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+//Register Mediator
+var assembly = new Assembly[]
+{
+    Assembly.GetExecutingAssembly(),
+    typeof(CreateDiscountHandler).Assembly
+};
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assembly));
 
+
+//Register Application Services
+builder.Services.AddScoped<IDiscount, DiscountRepository>();
+
+//Add Grop
+builder.Services.AddGrpc();
 var app = builder.Build();
+
+//migrate Database
+
+app.MigrateDatabase<Program>();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<DiscountService>();
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Communitation with GRPC server must be using GRPC Client");
+    });
+});
 
 app.Run();
