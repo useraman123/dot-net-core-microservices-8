7c65914118fefc1f17a8e76a57561c66d2df40c1
useraman123 <amank100003@gmail.com>
2025-05-05 12:11:42 +0530

    consuming the discount Grpc inside handler

--- Services/Basket/Basket.Application/Handler/CreateShoppingCartHandler.cs
+++ Services/Basket/Basket.Application/Handler/CreateShoppingCartHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.Application.Command;
+using Basket.Application.GrpcService;
 using Basket.Application.Mappers;
 using Basket.Application.Reponses;
 using Basket.Core.Repositories;
@@ -9,14 +10,22 @@ namespace Basket.Application.Handler;
 public class CreateShoppingCartHandler:IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
 {
     private readonly IBasketRepository _basket;
-    public CreateShoppingCartHandler(IBasketRepository basket)
+    //consuming the discount Grpc service
+    private readonly DiscountGrpcService _discountGrpcService;
+    public CreateShoppingCartHandler(IBasketRepository basket, DiscountGrpcService discountGrpcService)
     {
         _basket = basket;
+        _discountGrpcService = discountGrpcService;
     }
 
     public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
     {
         // TODO: WIll Be integrating Discount service here 
+        foreach (var item in request.Items)
+        {
+            var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            item.Price = item.Price - coupon.Amount;
+        }
         var shoppingCart = await _basket.UpsertBasket(new Core.Entities.ShoppingCart
         {
             UserName = request.UserName,
