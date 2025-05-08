a7bf91357fe0654a9bdfff4e8f2c0515c11b5527
useraman123 <amank100003@gmail.com>
2025-05-09 01:59:54 +0530

    context and Repository

---
+++ Services/Ordering/Order.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Core.Common;
+using Order.Core.Entities;
+
+namespace Order.Infrastructure.Data;
+
+public class OrderContext:DbContext
+{
+    public OrderContext(DbContextOptions<OrderContext> contextOptions):base(contextOptions)
+    {
+        
+    }
+
+    public DbSet<OrderEntity> Orders { get; set; }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangeOnSuccess,CancellationToken cancellationToken)
+    {
+        foreach (var entry in ChangeTracker.Entries<EntityBase>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedDate=DateTime.UtcNow;
+                    entry.Entity.CreatedBy = "Aman";
+                    break;
+
+                case EntityState.Modified:
+                    entry.Entity.CreatedDate = DateTime.UtcNow;
+                    entry.Entity.CreatedBy = "Aman";
+                    break;
+
+            }
+        }
+        return base.SaveChangesAsync(acceptAllChangeOnSuccess, cancellationToken);
+    }
+}
---
+++ Services/Ordering/Order.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,38 @@
+﻿using Order.Core.Common;
+using Order.Core.Repositories;
+using System.Linq.Expressions;
+
+namespace Order.Infrastructure.Repositories;
+
+public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
+{
+    public Task<T> AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
