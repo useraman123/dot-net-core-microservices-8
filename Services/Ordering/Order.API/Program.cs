e607a66e83e6f884d56c0c1408e9a59b61a31597
useraman123 <amank100003@gmail.com>
2025-05-11 22:40:45 +0530

    registering service in order api

--- Services/Ordering/Order.API/Program.cs
+++ Services/Ordering/Order.API/Program.cs
@@ -1,17 +1,39 @@
+using Asp.Versioning;
+using Order.Application.Extensions;
+using Order.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+#region Application Services
+//Application Services
+builder.Services.AddApplicationService();
+
+//Infra services
+builder.Services.AddInfrastructureService(builder.Configuration);
+#endregion
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//adding meta data into swagger
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Order API", Version = "v1" }); });
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
