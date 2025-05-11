df32cd9acb19a0f96da822989803ba8e0e926ea1
useraman123 <amank100003@gmail.com>
2025-05-11 14:02:56 +0530

    Adding Infraservice

---
+++ Services/Ordering/Order.Infrastructure/Extensions/InfraService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Order.Core.Repositories;
+using Order.Infrastructure.Data;
+using Order.Infrastructure.Repositories;
+
+namespace Order.Infrastructure.Extensions;
+
+public static class InfraService
+{
+    public static IServiceCollection AddInfrastructureService(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<OrderContext>(option => option.UseSqlServer(configuration.GetConnectionString("OrderConnectionString")));
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        return services;
+    }
+}
