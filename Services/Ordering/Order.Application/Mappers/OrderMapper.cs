9d8e30c2d60f9c510ae46b0f90ae799cee5fada8
useraman123 <amank100003@gmail.com>
2025-05-11 14:15:32 +0530

    Order Mapper

---
+++ Services/Ordering/Order.Application/Mappers/OrderMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Order.Application.Mappers;
+
+public static class OrderMapper
+{
+    //Lazy Mapping
+    private static readonly Lazy<IMapper> MapperInstance = new Lazy<IMapper>(() =>
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+            cfg.AddProfile<OrderMappingProfile>();
+        });
+        return config.CreateMapper();
+    });
+    // only initialised when needed
+    public static IMapper Mapper => MapperInstance.Value;
+}
---
+++ Services/Ordering/Order.Application/Mappers/OrderMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Order.Application.Responses;
+using Order.Core.Entities;
+
+namespace Order.Application.Mappers;
+
+public class OrderMappingProfile:Profile
+{
+    public OrderMappingProfile()
+    {
+        CreateMap<OrderEntity,OrderResponse>().ReverseMap();
+    }
+}
